569a4fdde1ca9b852fc2ea65097c93e86425bd13
Vasif Ali <aliw22933@gmail.com>
2025-12-15 21:51:58 +0530

    Initial Maven DevOps project

---
+++ src/main/java/com/devops/app/App.java
@@ -0,0 +1,8 @@
+package com.devops.app;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("DevOps Maven Project Running Successfully!");
+    }
+}
+      
